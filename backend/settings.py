410e9cc9c002610e70b50a67d56b4e4fc9f7422e
Aman Mishra <batmanmishra23@gmail.com>
2026-07-05 22:47:59 +0530

    Updated images to inline and added build script

--- backend/settings.py
+++ backend/settings.py
@@ -128,6 +128,15 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 
 import os
 
